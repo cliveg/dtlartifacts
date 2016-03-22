03e95bb1a1873239065614fefe38208784831fff
Clive Graven <clivegraven2@hotmail.com>
2016-03-21 19:41:24 -0600

    Upda

--- mdt/ChocolateyPackageInstaller.ps1
+++ mdt/ChocolateyPackageInstaller.ps1
@@ -251,7 +251,7 @@ function ConfigureMDT
 $url = "http://care.dlservice.microsoft.com/dl/download/6/2/A/62A76ABB-9990-4EFC-A4FE-C7D698DAEB96/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO"
 $output = "c:\win2012r2.iso"
 WriteLog $("Downloading:" + $output)
-#(New-Object System.Net.WebClient).DownloadFile($url, $output)
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
 
 # Setup-DeploymentShare
 WriteLog $("Setting up MDT Share")
