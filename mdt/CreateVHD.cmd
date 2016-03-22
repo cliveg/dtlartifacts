aded9196f9be004689d6557e97bc5496345a7dde
Clive Graven <clivegraven2@hotmail.com>
2016-03-21 23:05:45 -0600

    Update

--- mdt/CreateVHD.cmd
+++ mdt/CreateVHD.cmd
@@ -5,8 +5,10 @@ call DandISetEnv.bat
 CD "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\"
 call copype.cmd amd64 c:\winpe_amd64
 
-del d:\mdtazure.vhd
 diskpart /s C:\DeploymentShare\diskpart1.txt
 echo y|call MakeWinPEMedia /UFD C:\WinPE_amd64 V:
 copy C:\DeploymentShare\Boot\LiteTouchPE_x64.wim V:\sources\boot.wim /Y
 diskpart /s C:\DeploymentShare\diskpart2.txt
+dir c:\*.vhd
+echo **Finished Creating Custom VHD
+
